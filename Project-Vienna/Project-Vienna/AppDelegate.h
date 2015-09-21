1f9481ec63b96fd47fbe970c21f7ddfd92a121cb
Rodrigo Moura <rodmoura82@gmail.com>
2015-09-21 11:40:26 -0700

    Project created

---
+++ Project-Vienna/Project-Vienna/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project-Vienna
+//
+//  Created by Rodrigo Moura Gonçalves on 21/09/15.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project-Vienna/Project-Vienna/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Project-Vienna
+//
+//  Created by Rodrigo Moura Gonçalves on 21/09/15.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
