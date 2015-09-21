16a812a127e7c8f90880139b8dc495fa71b3bf41
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-21 15:01:29 -0700

    Adds attraction list custom cell and view controller for attractions list

---
+++ Project-Vienna/Project-Vienna/AttractionsListCell.h
@@ -0,0 +1,13 @@
+//
+//  AttractionsListCell.h
+//  Project-Vienna
+//
+//  Created by asu on 2015-09-21.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttractionsListCell : UITableViewCell
+
+@end
---
+++ Project-Vienna/Project-Vienna/AttractionsListCell.m
@@ -0,0 +1,23 @@
+//
+//  AttractionsListCell.m
+//  Project-Vienna
+//
+//  Created by asu on 2015-09-21.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import "AttractionsListCell.h"
+
+@implementation AttractionsListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
