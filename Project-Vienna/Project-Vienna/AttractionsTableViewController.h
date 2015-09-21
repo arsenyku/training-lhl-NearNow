bf1bff4fc4501fe0e2030e1f4731c98227144a10
Rodrigo Moura <rodmoura82@gmail.com>
2015-09-21 16:33:20 -0700

    Segue and AttractionViewController

---
+++ Project-Vienna/Project-Vienna/AttractionsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  AttractionsTableViewController.h
+//  Project-Vienna
+//
+//  Created by Rodrigo Moura Gonçalves on 21/09/15.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "City.h"
+#import "Location.h"
+
+@interface AttractionsTableViewController : UITableViewController
+
+@property (nonatomic, strong) City *city;
+
+@end
--- Project-Vienna/Project-Vienna/CityViewController.m
+++ Project-Vienna/Project-Vienna/CityViewController.m
@@ -7,12 +7,19 @@
 //
 
 #import "CityViewController.h"
+#import "AttractionsTableViewController.h"
+#import "City.h"
+#import "Location.h"
 
 @interface CityViewController ()
 
 @property (weak, nonatomic) IBOutlet UITableViewCell *londonCell;
 @property (weak, nonatomic) IBOutlet UITableViewCell *nycCell;
 @property (weak, nonatomic) IBOutlet UITableViewCell *vancouverCell;
+
+@property (strong, nonatomic) City *london;
+@property (strong, nonatomic) City *nyc;
+@property (strong, nonatomic) City *vancouver;
 
 @end
 
@@ -26,15 +33,20 @@
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
     if([segue.identifier isEqualToString:@"londonSegue"]){
-
+        AttractionsTableViewController *attractions = (AttractionsTableViewController *) segue.destinationViewController;
+        attractions.city = self.london;
     }
     else if ([segue.identifier isEqualToString:@"newYorkSegue"]){
-
+        AttractionsTableViewController *attractions = (AttractionsTableViewController *) segue.destinationViewController;
+        attractions.city = self.nyc;
     }
     else if ([segue.identifier isEqualToString:@"vancouverSegue"]){
-
+        AttractionsTableViewController *attractions = (AttractionsTableViewController *) segue.destinationViewController;
+        attractions.city = self.vancouver;
     }
     
 }
+
+
 
 @end
