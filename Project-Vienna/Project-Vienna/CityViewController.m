38bde3174923533ed366eff55cb02662a8a442c3
Rodrigo Moura <rodmoura82@gmail.com>
2015-09-21 16:07:09 -0700

    Cell and segue

--- Project-Vienna/Project-Vienna/CityViewController.m
+++ Project-Vienna/Project-Vienna/CityViewController.m
@@ -10,6 +10,9 @@
 
 @interface CityViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableViewCell *londonCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *nycCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *vancouverCell;
 
 @end
 
@@ -20,9 +23,18 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if([segue.identifier isEqualToString:@"londonSegue"]){
+
+    }
+    else if ([segue.identifier isEqualToString:@"newYorkSegue"]){
+
+    }
+    else if ([segue.identifier isEqualToString:@"vancouverSegue"]){
+
+    }
+    
 }
 
 @end
