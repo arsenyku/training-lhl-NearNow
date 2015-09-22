1637dfc5dba7bdbd26f577a92885b720ce92cba6
Rodrigo Moura <rodmoura82@gmail.com>
2015-09-21 19:02:55 -0700

    Views

---
+++ Project-Vienna/Project-Vienna/MapAttractionsViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  Project-Vienna
+//
+//  Created by Rodrigo Moura Gonçalves on 21/09/15.
+//  Copyright © 2015 Rodrigo Moura Gonçalves. All rights reserved.
+//
+
+#import "MapAttractionsViewController.h"
+
+@interface MapAttractionsViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation MapAttractionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
